c789a499d7991b24f2ad778154ed1786a1e05f86
hanjiwon1108 <jwjwhan1108@gmail.com>
2024-12-05 20:24:44 +0900

    MainLogo.svg 이미지 추가 및 App.js 파일 삭제

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import React from "react";
+
+export default function App() {
+  return;
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavItem = styled.div``;
